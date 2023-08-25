389d830f20b93fe44ec9d224f0906f261b9ec7ea
eljohnnyy <you@example.com>
2023-08-25 17:41:14 +0300

    best seller list view item part 1

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mybookly/core/utlis/styles.dart';
 
 
+import '../../../../../core/utlis/assets.dart';
 import 'custom_app_bar.dart';
 
 import 'featured_list_view.dart';
@@ -11,17 +12,40 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [CustomAppBar(), Padding(
-        padding: EdgeInsets.only(left: 14),
-        child: FeaturedBooksListView(),
+    return const  Padding(
+      padding: EdgeInsets.only(left: 24),
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [CustomAppBar(), FeaturedBooksListView(),
+        SizedBox(height: 50,),
+        Text('Best Seller',style: Styles.titleMedium,),
+        BestSellerListViewItem(),
+        ],
       ),
-      SizedBox(height: 50,),
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24),
-        child: Text('Best Seller',style: Styles.titleMedium,),
-      )
+    );
+  }
+}
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 125,
+          child: AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+            
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(AssetsData.testImage)))),
+            ),
+        ),
       ],
     );
   }
